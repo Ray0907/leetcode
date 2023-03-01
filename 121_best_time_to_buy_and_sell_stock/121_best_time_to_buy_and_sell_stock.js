33dd9dfdf4b4111bb8d9aff8288f5f3cb2271d28
Ray Tien <a0939552854@gmail.com>
2023-03-01 22:10:04 +0800

    Add 121.

---
+++ 121_best_time_to_buy_and_sell_stock/121_best_time_to_buy_and_sell_stock.js
@@ -0,0 +1,10 @@
+var maxProfit = function(prices) {
+    let results = 0
+    let lowest = prices[0];
+
+    for(let price of prices) {
+       lowest = price > lowest? lowest : price
+       results = results > price - lowest ? results: price - lowest 
+    }
+    return results
+};
