7f5610de3df61583a084e38926d80b40f461f78c
Ray Tien <a0939552854@gmail.com>
2023-10-28 11:59:44 +0800

    Add 208 (Medium).

---
+++ 208_implement_trie/208_implement_trie.py
@@ -0,0 +1,35 @@
+class Trie:
+
+    def __init__(self):
+        self.child = {}
+        
+
+    def insert(self, word: str) -> None:
+        node = self.child
+        for c in word:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        node['#'] = True
+
+    def search(self, word: str) -> bool:
+        node = self.child
+        for c in word:
+            if c not in node:
+                return False
+            node = node[c]
+        return '#' in node
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.child
+        for c in prefix:
+            if c not in node: return False
+            node = node[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
