48aee12d4314383cff5f6cfb7fbbbc545829ab16
Ray Tien <a0939552854@gmail.com>
2024-02-24 11:59:12 +0800

    Minor.

--- 572_subtree_of_another_tree/572_subtree_of_another_tree.py
+++ 572_subtree_of_another_tree/572_subtree_of_another_tree.py
@@ -5,27 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    # def isSameTree(self, tree1: Optional[TreeNode], tree2: Optional[TreeNode]) -> bool:
-    #     if not tree1 and not tree2:
-    #         return True
-    #     if not tree1 or not tree2 or tree1.val != tree2.val:
-    #         return False
-    #     return self.isSameTree(tree1.left, tree2.left) and self.isSameTree(tree1.right, tree2.right)
-    
-    # def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-    #     if not root:
-    #         return False
-    #     if self.isSameTree(root, subRoot):
-    #         return True
-    #     return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)    
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def serialize(node):
-            if not node:
-                return "null"
-            return "#" + str(node.val) + serialize(node.left) + serialize(node.right)
+        if not root:  # If root is None, return False
+            return False
+        # Check if the current subtree starting at root matches subRoot
+        return self.isSameTree(root, subRoot) or \
+               self.isSubtree(root.left, subRoot) or \
+               self.isSubtree(root.right, subRoot)
 
-        root_str = serialize(root)
-        subRoot_str = serialize(subRoot)
-
-        return subRoot_str in root_str
-        
+    def isSameTree(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if not root1 and not root2:  # Both trees are empty, hence same
+            return True
+        if not root1 or not root2:  # One of the trees is empty, not same
+            return False
+        # Check if current nodes have the same value and if subtrees are same
+        return root1.val == root2.val and \
+               self.isSameTree(root1.left, root2.left) and \
+               self.isSameTree(root1.right, root2.right)
