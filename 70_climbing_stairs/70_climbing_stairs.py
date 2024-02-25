a7a8851a65d4d6c1e40f0170e075a7fe9930d30e
Ray Tien <a0939552854@gmail.com>
2024-02-25 11:26:08 +0800

    Add 70 (easy).

--- 70_climbing_stairs/70_climbing_stairs.py
+++ 70_climbing_stairs/70_climbing_stairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # Fibonacci
+        #solution 1.
         if n <= 3:
             return n
         n1, n2 = 2, 3
@@ -10,3 +11,15 @@ class Solution:
             n1 = n2
             n2 = temp
         return n2
+
+        # solution 2
+        if n< 3:
+            return n
+        
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
